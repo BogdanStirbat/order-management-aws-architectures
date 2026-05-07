4dd03de55177723e8a98144d8735175c7635ed5c
Bogdan Stirbat <bogdan.stirbat@gmail.com>
2026-05-07 23:58:46 +0300

    unused import

--- 04-lambda-aurora/app/lambdaaurora/src/main/java/com/order/management/lambdaaurora/web/Router.java
+++ 04-lambda-aurora/app/lambdaaurora/src/main/java/com/order/management/lambdaaurora/web/Router.java
@@ -1,6 +1,5 @@
 package com.order.management.lambdaaurora.web;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
